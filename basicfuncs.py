9decdca42581d3eeba95d7b6ab87ea28bffd6fef
Goti <mail@bourier.biz>
2017-03-07 14:14:08 +0100

    added 'game-over'-screen. replay and exit functionality

--- basicfuncs.py
+++ basicfuncs.py
@@ -4,9 +4,9 @@ import random
 import numpy as np
 from PIL import Image
 
-def initfield(Display, field, specialpos, black, gray, display_width):
+def initfield(Display, field, specialpos, black, gray, display_width, bgcolor):
 	global position
-	Display.fill((225,225,250))
+	Display.fill(bgcolor)
 	textmaker(Display, display_width/2, 10, 30, 30, 'WoGoTo', (0,0,0), 30, 0)	
 	positions = []
 	#make field
@@ -52,10 +52,10 @@ def updatefield(Display, field, cities, players, specialpos, CP, playerpos, play
 			textmaker(Display, 395, 390, 30, 30, 'Start', (255,255,255), 20, 0)	
 	
 	#keep other player on old city
-	if CP == 0 and players[1][2] == playerpos:
-		pygame.draw.circle(Display, (200,200,255), [80+55*j,75+55*i], 10, 0)
-	elif CP == 1 and players[0][2] == playerpos:
-		pygame.draw.circle(Display, (255,200,200), [80+55*j,75+55*i], 10, 0)
+	if CP == 0 and players[1][2] == playerposold:
+		pygame.draw.circle(Display, (255,150,150), [80+55*j2,75+55*i2], 10, 0)
+	elif CP == 1 and players[0][2] == playerposold:
+		pygame.draw.circle(Display, (150,150,255), [80+55*j2,75+55*i2], 10, 0)
 	
 	#update city at new position
 	if cities[playerpos][1] == 0:
@@ -67,7 +67,8 @@ def updatefield(Display, field, cities, players, specialpos, CP, playerpos, play
 	if CP == 0:
 		pygame.draw.circle(Display, (150,150,255), [80+55*j,75+55*i], 10, 0)	
 	else:
-		pygame.draw.circle(Display, (255,150,150), [80+55*j,75+55*i], 10, 0)	
+		pygame.draw.circle(Display, (255,150,150), [80+55*j,75+55*i], 10, 0)
+
 	
 	#write toll / mult
 	if cities[playerpos][1] == 0 and playerpos not in specialpos:
@@ -82,7 +83,70 @@ def updatefield(Display, field, cities, players, specialpos, CP, playerpos, play
 def levelup_cities(CP, cities, specialpos, red, blue):
 	return
 	
-
+def end_game(Display, clock, CP, bgcolor, black, grey, lightgrey, red, blue):
+	color = bgcolor
+	crashed = False
+	flash = 'on'
+	endevent = pygame.USEREVENT + 1
+	endevent2 = pygame.event.Event(endevent)
+	pygame.event.post(endevent2)
+	while not crashed:
+		for event in pygame.event.get():
+			Display.fill(bgcolor)
+			
+			#check mouse on replay/exit
+			mouse = pygame.mouse.get_pos()
+			if 255+80 > mouse[0] > 255 and 260+50 > mouse[1] > 260:
+				pygame.draw.rect(Display, lightgrey,(255,260,80,50))
+				textmaker(Display, 280, 270, 30, 30, 'Exit', black, 20, 0)
+			else:
+				pygame.draw.rect(Display, grey,(255,260,80,50))
+				textmaker(Display, 280, 270, 30, 30, 'Exit', black, 20, 0)			
+			if 185+80 > mouse[0] > 185 and 260+50 > mouse[1] > 270:
+				pygame.draw.rect(Display, lightgrey,(160,260,80,50))
+				textmaker(Display, 185, 270, 30, 30, 'Replay', black, 20, 0)
+			else:
+				pygame.draw.rect(Display, grey,(160,260,80,50))
+				textmaker(Display, 185, 270, 30, 30, 'Replay', black, 20, 0)				
+			pygame.display.update()
+			
+			#replay or exit 
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				if event.button == 1:
+					if 255+80 > mouse[0] > 255 and 260+50 > mouse[1] > 260:
+						crashed = True
+						exitted = True
+						return crashed
+					elif 185+80 > mouse[0] > 185 and 260+50 > mouse[1] > 270:
+						crashed = True
+						exitted = False
+						return exitted
+					else:
+						pass
+					
+					
+					
+			# Display endscreen
+			if event.type == 25:		
+				if flash == 'off':
+					color = blue
+					flash = 'on'				
+				elif flash == 'on':
+					color = red
+					flash = 'off'	
+				textmaker(Display, 230,140, 30, 30, 'GAME OVER', color, 30, 0)
+				textmaker(Display, 230,190, 30, 30, 'Player: '+str(CP+1)+' lost', color, 30, 0)				
+				pygame.display.update()
+				pygame.event.pump()
+				pygame.event.clear()
+				endevent = pygame.USEREVENT + 1
+				endevent2 = pygame.event.Event(endevent)
+				pygame.event.post(endevent2)
+				clock.tick(5) 
+				
+	
+	
+	
 # text related functions	
 def text_objects(text, font, color):
     textSurface = font.render(text, True, color)
